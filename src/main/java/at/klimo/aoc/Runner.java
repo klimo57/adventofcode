904b8417f1def8fb97d6b9de187004d8437bdccf
klimo57 <stefan.stanzer@gmail.com>
2023-12-01 22:57:18 +0100

    added comments for documentation

--- src/main/java/at/klimo/aoc/Runner.java
+++ src/main/java/at/klimo/aoc/Runner.java
@@ -6,10 +6,17 @@ import java.time.ZoneId;
 import java.util.Arrays;
 import java.util.TimeZone;
 
+/**
+ * Run <code>mvn compile -q exec:java</code> to run today's puzzle.
+ * To implement a solution, create a class implementing the {@link Solution} interface in the package for the
+ * respective year (e.g. y2023) with the name "Solution{day}", e.g. Solution1.
+ * The solution for the puzzle will be printed on the console
+ */
 public class Runner {
 
     public static void main(String[] args) {
         try {
+            // get the current date in EST (EST is used by the creator of AOC) and print today's solution
             var date = LocalDate.now(TimeZone.getTimeZone("EST").toZoneId());
             int year = date.getYear();
             int day = date.getDayOfMonth();
--- src/main/java/at/klimo/aoc/y2023/Solution1.java
+++ src/main/java/at/klimo/aoc/y2023/Solution1.java
@@ -29,25 +29,45 @@ public class Solution1 implements Solution {
             this.line = line;
         }
 
+        /**
+         * returns the "calibration code" of a line. the calibration code consists of the first and the last digit
+         * in the line, either written out in letters (e.g. "one") or as the digit itself (1)<br><br>
+         *
+         * for efficiency, the line is looked at from both ends. if a digit is found, its index is compared to any
+         * digit already found to find out which one is first.<br><br>
+         *
+         * for example, when looking at the string <i>1asd3f</i>, the first and last characters are '1' and 'f'.
+         * since 'f' is not a digit, it is ignored. '1' on the other hand will be noticed as first <b>and</b> last digit. <br>
+         * the next digits are 'a' and '3'. again, 'a' can be discarded, but '3' is a digit. by comparing the indizes
+         * the digits where found at, it can be determined that '3' is not the first digit, but will indeed replace '1'
+         * as the last digit that can be found. <br>
+         * through the fact that there is no character with an index smaller than our already determined first digit or
+         * greater than the last one, the calculation is already finished
+         */
         public int calibrate(String line, int i, int j) {
+            // throw on empty lines
             if(line.length() < 1) {
                 throw new ImplementationException("no digits found in line " + this.line);
             }
 
+            // calculate the indizes and substring range for the next run
             int nextI = i + 1;
             int nextJ = j - 1;
             int beginIndex = 1;
             int endIndex = line.length() - 1;
 
+            // look for digits
             var first = new CalibrationCharacter(line.charAt(0), i);
             var ldStart = LetteredDigit.fromStringStart(line);
             var last = new CalibrationCharacter(line.charAt(line.length() - 1), j);
             var ldEnd = LetteredDigit.fromStringEnd(line);
 
+            // check if we still need to look for a first digit
             if(alpha == null || alpha.index > i) {
                 if(first.isValid()) {
                     alpha = first;
                 } else if(ldStart.isPresent()) {
+                    // if the digit was written out, the indizes need to be corrected
                     var ld = ldStart.get();
                     alpha = new CalibrationCharacter(ld.digit, i);
                     beginIndex = ld.name().length();
@@ -55,6 +75,7 @@ public class Solution1 implements Solution {
                 } else if(last.isValid() && (alpha == null || alpha.index > j)) {
                     alpha = last;
                 } else if(ldEnd.isPresent()) {
+                    // use j as the index here to make sure we catch digits from the other side
                     alpha = new CalibrationCharacter(ldEnd.get().digit, j);
                 }
             }
@@ -62,6 +83,7 @@ public class Solution1 implements Solution {
                 if(last.isValid()) {
                     omega = last;
                 } else if(ldEnd.isPresent()) {
+                    // if the digit was written out, the indizes need to be corrected
                     var ld = ldEnd.get();
                     omega = new CalibrationCharacter(ld.digit, j);
                     endIndex = line.length() - ld.name().length();
@@ -69,9 +91,15 @@ public class Solution1 implements Solution {
                 } else if(first.isValid() && (omega == null || omega.index < i)) {
                     omega = first;
                 } else if(ldStart.isPresent()) {
+                    // use i as the index here to make sure we catch digits from the other side
                     omega = new CalibrationCharacter(ldStart.get().digit, i);
                 }
             }
+            /*
+             * we are finished if we have both digits, and
+             *  a) there are no more digits or
+             *  b) the indizes of our digits are further on the outsides of the line than all indizes yet to come
+             */
             if(alpha != null && omega != null && (line.length() <= 2 || nextI >= alpha.index && nextJ <= omega.index)) {
                 return Integer.parseInt(String.valueOf(new char[]{alpha.value, omega.value}));
             }
@@ -79,6 +107,10 @@ public class Solution1 implements Solution {
         }
     }
 
+    /**
+     * A calibration character contains of a value and the index on which it was found.
+     * It is valid if it is a digit.
+     */
     private static class CalibrationCharacter {
         final char value;
         final int index;
@@ -102,6 +134,9 @@ public class Solution1 implements Solution {
             this.digit = digit;
         }
 
+        /**
+         * get the lettered digit at the start of a string (if there is one)
+         */
         static Optional<LetteredDigit> fromStringStart(String s) {
             final var sanitized = s.trim().toLowerCase();
             return Arrays.stream(values())
@@ -109,6 +144,9 @@ public class Solution1 implements Solution {
                 .findAny();
         }
 
+        /**
+         * get the lettered digit at the end of a string (if there is one)
+         */
         static Optional<LetteredDigit> fromStringEnd(String s) {
             final var sanitized = s.trim().toLowerCase();
             return Arrays.stream(values())

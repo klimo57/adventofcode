ff1fd183d86d6befe7f64f47eb8ff9978e7174fb
klimo57 <stefan.stanzer@gmail.com>
2023-12-02 21:55:17 +0100

    added comments

--- src/main/java/at/klimo/aoc/y2023/Solution2.java
+++ src/main/java/at/klimo/aoc/y2023/Solution2.java
@@ -7,14 +7,24 @@ import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toMap;
 
 public class Solution2 implements Solution<Integer> {
+
+    /**
+     * puzzle 1 looks for games with a bag that could contain 12 red, 13 green and 14 blue cubes
+     * the solution is the sum of the games ids
+     */
     @Override
     public Integer puzzle1(String[] input) throws ImplementationException {
         return stream(input)
             .map(Solution2::readGame)
-            .mapToInt(Solution2::applyRules)
+            .filter(game -> game.red <= 12 && game.green <= 13 && game.blue <= 14)
+            .mapToInt(Game::id)
             .sum();
     }
 
+    /**
+     * puzzle 2 looks for the fewest number of cubes of each color that could have been in the bag
+     * the solution is the sum of the products of the number of cubes of each game
+     */
     @Override
     public Integer puzzle2(String[] input) throws ImplementationException {
         return stream(input)
@@ -23,6 +33,10 @@ public class Solution2 implements Solution<Integer> {
             .sum();
     }
 
+    /**
+     * reads a line and converts it to a game with an id and the fewest number of cubes of each color
+     * that could have been in the bag
+     */
     private static Game readGame(String line) {
         String[] parts = line.split(":");
         int id = Integer.parseInt(parts[0].substring(parts[0].indexOf("Game ") + 5));
@@ -38,6 +52,9 @@ public class Solution2 implements Solution<Integer> {
             .orElseThrow();
     }
 
+    /**
+     * returns a game for each of the shown cubes
+     */
     private static Game readColors(String randomCubes, int id) {
         var cubesByColor = stream(randomCubes.split(","))
             .map(String::trim)
@@ -53,11 +70,18 @@ public class Solution2 implements Solution<Integer> {
         );
     }
 
-    private static int applyRules(Game game) {
-        return game.red <= 12 && game.green <= 13 && game.blue <= 14 ? game.id : 0;
-    }
-
+    /**
+     * the data of a game
+     *
+     * @param id    the game's id
+     * @param red   the number of red cubes
+     * @param green the number of green cubes
+     * @param blue  the number of blue cubes
+     */
     private record Game(int id, int red, int green, int blue) {
+        /**
+         * @return the product of the number of red, green and blue cubes
+         */
         private int power() {
             return red * green * blue;
         }
